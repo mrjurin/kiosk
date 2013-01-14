97be8e3a0b6b8396542ae5a8661077478af31ce9
Deepan <blinduck@gmail.com>
2013-01-14 17:35:44 +0800

    added app and bootstrap static files

--- kiosk/urls.py
+++ kiosk/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns('app.views',
     # Examples:
     # url(r'^$', 'kiosk.views.home', name='home'),
     # url(r'^kiosk/', include('kiosk.foo.urls')),
@@ -14,4 +14,5 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^$','index' )
 )
