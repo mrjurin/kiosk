57fd2bf1990514b260c194edb845de5060d97b2c
Deepan <blinduck@gmail.com>
2013-02-14 13:53:47 +0800

    push to github

--- app/static/v1/js/timeline.js
+++ app/static/v1/js/timeline.js
@@ -22,22 +22,30 @@ $(document).ready(function(){
 
 		if (x>8060) {
 			$('#parliament').transition({y:-346},500);
+			$('.palmtrees').fadeIn(800);
 		}
 
 		if (x>8900) {
 			$('#excavator').addClass('excavator-animate');
 			window.setTimeout(function(){$('#ex-arm').
 				addClass('arm-animate');},1000);
+			window.setTimeout(digSoil,1200);
+			window.setTimeout(function(){
+				$('#foundation').fadeIn(500);
+			},1700);
+			window.setTimeout(function(){
+				$('#foundation').fadeIn(500);
+			},2200);
+			window.setTimeout(cranePop,2500);
+			window.setTimeout(pilesUp,3500);
+			window.setTimeout(craneLoad,3500);
 
 		}
 
-		if (x>9000) {
-			$('#dugoutsoil').transition({
-				y:80,
-				
-				complete: animateFoundation()
-			},500);
+		if (x>9200) {
+			engineeringBuildings();
 		}
+
 		
 	});
 
@@ -50,10 +58,34 @@ function animateLine (bullet) {
 		function () {$('.bullet-box, .bullet-text', '#' + bullet).fadeIn(600);}, 1000);
 };
 
-
-function animateFoundation () {
-	return;
+function digSoil() {
+	$('#dugoutsoil').transition({y:80},500);
 }
+
+function cranePop () {
+	$('#crane-div').transition({y:-795}, 1000);
+}
+
+function pilesUp() {
+	$('#piles').addClass("animatepiles");
+
+}
+
+function craneLoad () {
+	$('#craneload').addClass('animateCraneLoad');
+}
+
+function engineeringBuildings () {
+	$('#e2block').transit({y:-336},500);
+	window.setTimeout(function(){
+		$('#eblock').transit({y:-336},500);
+	},500);
+	window.setTimeout(function(){
+		$('#eablock').transit({y:-336},500);
+	},1000)
+
+}
+
 
 
 
